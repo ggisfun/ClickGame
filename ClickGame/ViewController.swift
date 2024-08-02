8c17a5b32bd8542a7ef4c89864c7987b012bc84e
Adam <bgisgone@gmail.com>
2024-08-02 13:01:06 +0800

    Initial Commit

---
+++ ClickGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickGame
+//
+//  Created by Adam Chen on 2024/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
